e21ce107369ed55ac2a9f909f62def8f9a4541fb
smonish64 <smonishsudhakar@gmail.com>
2025-09-17 21:48:33 +0530

    Update bro.py

--- bro.py
+++ bro.py
@@ -1,1 +1,12 @@
-print('welcome')
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Azure!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Azure provides PORT
+    app.run(host='0.0.0.0', port=port)
